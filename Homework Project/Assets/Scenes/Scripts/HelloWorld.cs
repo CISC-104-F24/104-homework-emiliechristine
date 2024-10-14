b6a4c4a33b94213598a0717b92b7fd7ce7fcf166
emiliechristine <milktea@udel.edu>
2024-10-14 13:31:11 -0400

    Update HelloWorld.cs

--- Homework Project/Assets/Scenes/Scripts/HelloWorld.cs
+++ Homework Project/Assets/Scenes/Scripts/HelloWorld.cs
@@ -7,7 +7,14 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-    Debug.Log("Hello, world!" );    
+    Debug.Log("Hello, world!" ); 
+    Debug.LogWarning("this is a warning message!")
+    //   "Author: Emilie Buchanan"
+    /*
+    I love playing horror games so I would like to create one
+    Who go you into playing video gaming?
+    How do game designers create the terrain/map?
+    */
     }
 
     // Update is called once per frame
